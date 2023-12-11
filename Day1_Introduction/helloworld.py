4a0b7eb208fca330759aa1a14e37ad5f10b934e0
Yusufyyakubu <110755736+Yusufyyakubu@users.noreply.github.com>
2023-12-11 13:51:34 +0100

    Updated helloworld.py

--- Day1_Introduction/helloworld.py
+++ Day1_Introduction/helloworld.py
@@ -1,1 +1,41 @@
-Hello world
+# introduction
+
+# day1
+
+print(3 + 4)   #addition(+)
+print(3 - 4)   #subtraction(-)
+print(3 * 4)   #multiplication(*)
+print(3 % 4)   #modulus(%)
+print(3 / 4)   #division(/)
+print(3 ** 4)  #exponential(**)
+print(3 // 4)  #floor division operator(//)
+
+Name = "Yusuf Yakubu Yusuf"
+Family_Name = "Yusuf"
+Country = "Nigeria"
+Feedback = "ArewaDS I am enjoying 30 days of python"
+print(Name, Family_Name, Country, Feedback)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['MECCA', 'ABUJA', 'CaIRO']))
+print(type(Name))
+print(type(Family_Name))
+print(type(Country))
+
+
+
+Number = (Integer 5) (Float 3.142) (Complex 2 + 4j)
+
+String = "Yusuf"
+Boolean = True or False
+List = [2, 5]
+Tuple = (3.142, 5.666)
+Set = {1, 2, 3}
+Dictionary = {'course' : 'computer science'}
+print(Number)
+
+
+
