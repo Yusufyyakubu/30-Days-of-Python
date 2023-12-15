8c9acc4fe2f8df9dd0f57a555532bd2d67659863
Yusufyyakubu <110755736+Yusufyyakubu@users.noreply.github.com>
2023-12-15 10:09:50 +0100

    Update 17_Day_Exception_handling.py

--- 17_Day_Exception_handling/17_Day_Exception_handling.py
+++ 17_Day_Exception_handling/17_Day_Exception_handling.py
@@ -1,1 +1,13 @@
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+# Unpack the first five countries
+nordic_countries = names[:5]
+
+# Unpack Estonia and Russia
+es, ru = names[5], names[6]
+
+# Display the results
+print("Nordic Countries:", nordic_countries)
+print("Estonia:", es)
+print("Russia:", ru)
 
